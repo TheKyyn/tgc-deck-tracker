d7c9f52882a3d83e5345544dfc001267e3b74f50
Kyyn <maxtheo155@icloud.com>
2024-11-15 17:57:42 +0100

    Fix Mixed Content and HTTPS issues

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js'
-            ],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        https: true,
-    },
+    base: process.env.APP_URL + '/',
 });
