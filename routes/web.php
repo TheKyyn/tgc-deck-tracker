656a62139ae8264ca4a151aa47434a1a4c315af7
Kyyn <maxtheo155@icloud.com>
2024-11-15 16:13:02 +0100

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\DeckStats;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', DeckStats::class);
